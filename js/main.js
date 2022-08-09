63756eb0354a5a4e6eee5fe40ea3082b80c6b3f6
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-10 08:50:22 +0900

    [Add]dataオプションを実装

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 const app = Vue.createApp({
   data: () =>({
-
+    items: null,
+    keyword: '',
+    message: ''
   }),
 
   watch: {
 
   },
 
-  mounted: {
+  mounted: function() {
 
   },
 
   methods: {
-    
+
   }
 })
 
